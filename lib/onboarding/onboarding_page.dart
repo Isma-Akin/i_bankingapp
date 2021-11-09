bbc200245186f171e34c60430c6d231173f96e6b
Isma-Akin <aakinsemoyin@hotmail.com>
2021-11-09 16:27:21 +0000

    added a background color and body

--- lib/onboarding/onboarding_page.dart
+++ lib/onboarding/onboarding_page.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 
     class OnboardingPage extends StatelessWidget {
       @override
-      Widget build(BuildContext context)  {
-        return Container();
-        }
+      Widget build(BuildContext context) {
+        return Scaffold(
+          backgroundColor: Colors.indigo,
+          body: false,
+          child: Column
+        );
+      }
     }
