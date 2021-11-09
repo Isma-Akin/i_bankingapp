ee885855a7660589157c77e122aa0f6378d126bb
Isma-Akin <aakinsemoyin@hotmail.com>
2021-11-09 16:55:22 +0000

    added assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:i_bankingapp/onboarding/onboarding_page.dart';
 
 void main() =>  runApp(MyApp());
 
--- lib/onboarding/onboarding_page.dart
+++ lib/onboarding/onboarding_page.dart
@@ -6,7 +6,12 @@ import 'package:flutter/material.dart';
         return Scaffold(
           backgroundColor: Colors.indigo,
           body: false,
-          child: Column
+          child: Column(children: [
+            SizedBox(height: 80),
+            Image.asset(assets/image_onboarding.png',
+                width: 355
+            )
+          ],)
         );
       }
     }
