e4560d75550dcd9799e9fc2b7ed444f48d6c8b06
Isma-Akin <aakinsemoyin@hotmail.com>
2021-11-13 14:11:40 +0000

    refactor: background color to indigo

--- lib/onboarding/onboarding_page.dart
+++ lib/onboarding/onboarding_page.dart
@@ -2,79 +2,77 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:i_bankingapp/onboarding/home_page.dart';
+import 'package:i_bankingapp/onboarding/home/home_page.dart';
 
 class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.indigo,
-        body: SafeArea(
-            bottom: false,
-            child: Stack(
-              children: [
-                Positioned(
-                  bottom: -20,
-                  left: -60,
-                  child: Image.asset('image_hand.png'),
-                  width: 250,
+      backgroundColor: Colors.indigo,
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: [
+            Positioned(
+              bottom: -20,
+            left: -60,
+            child: Image.asset('image_hand.png',
+            width: 250,)
+              ,),
+            Center(
+              child: Column(children: [
+                SizedBox(height: 80,),
+                Image.asset(
+                  'image_onboarding.png',
+                width: 355, ),
+                SizedBox(height: 80,),
+                Text('A better way for you',
+                  style: TextStyle(fontSize: 24,
+                  fontWeight: FontWeight.w300),
                 ),
-                Center(
-                  child: Column(children: [
-                    SizedBox(
-                      height: 80,
-                    ),
-                    Image.asset(
-                      'image_onboarding.png',
-                      width: 355,
-                    ),
-                    SizedBox(
-                      height: 90,
-                    ),
-                    Text(
-                      'A pristine way for you',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.white),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      'What can we say if there is an app that you can transfer '
-                      'money without getting any fees?\n With promise',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w300, color: Colors.white),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                      GestureDetector(
-                        onTap: () {Navigator.push(
-                        context,
-                      MaterialPageRoute(
-                        builder: (context) => HomePage(),
-                      ),
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 48,
-                          vertical: 14,
-                        )),
-                        decoration: BoxDecoration(
-                          color: Colors.orange,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Text(
-                          'Ok sign me up',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w400, color: Colors.black),
-                        ),
-                      ),
-                    ]),
-                  ]),
+                SizedBox(height: 10),
+                Text('What can we say if there is an app\n that you can transfer money without getting any fee? Promises',
+                style: TextStyle(color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w300),
+                  textAlign: TextAlign.center,
                 ),
+                SizedBox(
+                  height: 70,
+                ),
+                Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 48,
+                        vertical: 14,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.horizontal(
+                          left: Radius.circular(20),
+                        )
+                      ),
+                      child: Text('Ok, Bring me in',
+                      style: TextStyle(
+                          fontSize: 18,
+                      fontWeight: FontWeight.w300),
+                      ),
+                    ),
+                  )
+                ],)
               ],
-            )));
+
+                ),
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
