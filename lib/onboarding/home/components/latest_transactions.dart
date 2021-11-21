4cd4daaf1a168ad3ac82480852fefd2a66b66300
Isma-Akin <aakinsemoyin@hotmail.com>
2021-11-21 14:59:19 +0000

    add: currency text input formatter refactor: FlutterMoneyFormatter => CurrencyTextInputFormatter

--- lib/onboarding/home/components/latest_transactions.dart
+++ lib/onboarding/home/components/latest_transactions.dart
@@ -1,11 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
 
 class LatestTransactions extends StatelessWidget {
   const LatestTransactions({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Widget transactionItem({
+      required String iconUrl,
+      required String title,
+      required String time,
+      required double price,
+    }){
+     return Container(
+        padding: EdgeInsets.all(15),
+        margin: EdgeInsets.only(
+          bottom: 16,
+        ),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(18,)
+        ),
+        child: ListTile(
+          contentPadding: EdgeInsets.zero,
+          leading: Image.asset(iconUrl,
+          ),
+          title: Text(
+              title,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              )
+          ),
+          subtitle: Text(
+            time,
+            style: TextStyle(
+              fontSize: 12,
+              color: Colors.grey,
+            ),
+          ),
+          trailing: Text(
+            CurrencyTextInputFormatter(
+                amount: price,
+                settings: MoneyFormatterSettings(
+                    thousandSeparator ',',
+                    decimalSeparator: '.'.,
+                    fractionDigits: 2),
+                    ).output.symbolOnLeft.toString(),
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.red,
+            ) ,
+          ),
+
+        ),
+      );
+      } {
+    }
     return Container(
       margin: EdgeInsets.only(
         top: 30,
@@ -26,44 +79,10 @@ class LatestTransactions extends StatelessWidget {
           SizedBox(
             height: 12,
           ),
-          Container(
-            padding: EdgeInsets.all(15),
-            margin: EdgeInsets.only(
-              bottom: 16,
-            ),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(18,)
-            ),
-            child: ListTile(
-              contentPadding: EdgeInsets.zero,
-              leading: Image.asset('icon_transaction1.png',
-              ),
-              title: Text(
-                  'Cakwe Medan',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                  )
-              ),
-              subtitle: Text(
-                'Today, 10 AM',
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.grey,
-                ),
-              ),
-              trailing: Text(
-                  '-\$920.00',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.red,
-                ) ,
-              ),
+          transactionItem(iconUrl: ),
+          transactionItem(price: null),
+          transactionItem(time: ''),
 
-            ),
-          )
       ],),
     );
   }
